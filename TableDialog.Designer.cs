e272e59fcd0d236e3f365de9bc6ef89881283e56
objz <me@objz.dev>
2026-03-12 20:32:49 +0100

    Remove unused using and reduce dialog client size

--- TableDialog.Designer.cs
+++ TableDialog.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace PowerImport
+namespace PowerImport
 {
     partial class TableDialog
     {
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(280, 130);
             this.Text = "TableDialog";
         }
 
